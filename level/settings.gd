69a8598526b7728d500d16f3553f76e759d30fea
Victor <victornbarrera@gmail.com>
2021-11-06 13:01:36 -0700

    fix sound bug

--- level/settings.gd
+++ level/settings.gd
@@ -5,8 +5,7 @@ const SFX_INDEX = 2
 
 func _ready():
 	$BackgroundPanel.visible = false
-	$BackgroundPanel/MusicVSlider.value = AudioServer.get_bus_volume_db(MUSIC_INDEX)
-	$BackgroundPanel/SFXVSlider.value = AudioServer.get_bus_volume_db(SFX_INDEX)
+	load_sound_settings()
 
 func did_toggle_settings():
 	$BackgroundPanel.visible = !$BackgroundPanel.visible
@@ -33,6 +32,16 @@ func hide_quit():
 	$BackgroundPanel/ResetButton.hide()
 	$BackgroundPanel/SaveAndQuitButton.disabled = true
 	$BackgroundPanel/ResetButton.disabled = true
+
+func load_sound_settings():
+	if AudioServer.is_bus_mute(MUSIC_INDEX):
+		$BackgroundPanel/MusicVSlider.value = -24
+	else:
+		$BackgroundPanel/MusicVSlider.value = AudioServer.get_bus_volume_db(MUSIC_INDEX)
+	if AudioServer.is_bus_mute(SFX_INDEX):
+		$BackgroundPanel/SFXVSlider.value = -24
+	else:
+		$BackgroundPanel/SFXVSlider.value = AudioServer.get_bus_volume_db(SFX_INDEX)
 
 func _on_SaveAndQuitButton_pressed():
 	get_tree().call_group("level", "did_save_and_quit_game")
